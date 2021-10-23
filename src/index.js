1c6b09c808cc6a522a27d306e399eae35e625411
boyun <boyoon54@gmail.com>
2021-10-23 17:43:38 +0900

    show background images
    - part of page1
    - not apply real images

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import SelectBackground from './SelectBackground';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SelectBackground />
   </React.StrictMode>,
   document.getElementById('root')
 );
